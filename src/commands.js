75e145238307fdbb0842a2d2b8b9a16e4489659b
Edward <palo4kazefira@gmail.com>
2021-11-09 00:37:02 +0200

    Delete commands.js

--- src/commands.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.commands = void 0;
-exports.commands = [
-    { command: '/start', description: 'Start conversation' },
-    { command: '/info', description: 'Get your information' },
-    { command: '/game', description: 'Random number game' },
-];
