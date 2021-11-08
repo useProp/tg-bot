e86421811801893247b52ba768e15aa9834fc793
Mark <mark.pilihri@gmail.com>
2021-11-08 05:06:11 +0200

    added db

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
 const TelegramApi = require('node-telegram-bot-api')
+
 const { gameOptions, againOptions } = require('./options')
+const sequelize = require('./db')
+const User = require('./models/User')
 
 const token = '2069355701:AAG4P2VF5H1Y_S4cvq0MoItaTZo0FZOAjqM'
 const bot = new TelegramApi(token, { polling: true })
 const chats = {}
 
-const startGame = async chatId => {
-  bot.setMyCommands([
-    {command: '/start', description: 'Start conversation'},
-    {command: '/info', description: 'Get your information'},
-    {command: '/game', description: 'Random number game'},
-  ])
+const dbConnection = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+  } catch (e) {
+    console.log(`DB connection error: ${e}`)
+  }
+}
 
+const startGame = async chatId => {
   await bot.sendMessage(chatId, 'Guess a number: Try to guess a random number from 0 to 9')
   const randomNumber = Math.floor(Math.random() * 10)
   chats[chatId] = randomNumber
   await bot.sendMessage(chatId, 'You can start :)', gameOptions)
 }
 
-const start = () => {
+const start = async () => {
+  await dbConnection()
+
+  bot.setMyCommands([
+    {command: '/start', description: 'Start conversation'},
+    {command: '/info', description: 'Get your information'},
+    {command: '/game', description: 'Random number game'},
+  ])
+
+
   bot.on('message', async msg => {
     const text = msg.text
     const chatId = msg.chat.id
 
-    if (text === '/start') {
-      await bot.sendSticker(chatId, 'https://tlgrm.eu/_/stickers/e1d/7e1/e1d7e170-0e83-4b0e-9bfd-83150380e736/2.webp')
-      return bot.sendMessage(chatId, `Hello, I am FirstBot :)`)
-    }
+    try {
+      if (text === '/start') {
+        const user = await User.findOne({ chatId })
+        console.log(`user: ${user}`)
+        if (!user) {
+          await User.create({ chatId })
+        }
+        await bot.sendSticker(chatId, 'https://tlgrm.eu/_/stickers/e1d/7e1/e1d7e170-0e83-4b0e-9bfd-83150380e736/2.webp')
+        return bot.sendMessage(chatId, `Hello, I am FirstBot :)`)
+      }
 
-    if (text === '/info') {
-      return bot.sendMessage(chatId, `Your name is ${msg.from.first_name} ${msg.from.username}`)
-    }
+      if (text === '/info') {
+        const user = await User.findOne({ chatId })
+        console.log(user)
+        return bot.sendMessage(chatId, `Your score is r: ${user.right} w: ${user.wrong}`)
+      }
 
-    if (text === '/game') {
-      return startGame(chatId)
+      if (text === '/game') {
+        return startGame(chatId)
+      }
+    } catch (e) {
+      console.log(e)
+      return bot.sendMessage(chatId, 'Woops... Something went wrong :(')
     }
 
     return bot.sendMessage(chatId, 'I don\'t understand you')
@@ -47,11 +74,16 @@ const start = () => {
       return startGame(chatId)
     }
 
+    const user = await User.findOne({ chatId })
     if (chats[chatId] === Number(data)) {
-      return bot.sendMessage(chatId, `Congratulations, ${data} it\'s right answer! :)`, againOptions)
+      user.right += 1
+      await bot.sendMessage(chatId, `Congratulations, ${data} it\'s right answer! :)`, againOptions)
     } else {
-      return bot.sendMessage(chatId, 'Nope, try again :)', againOptions)
+      user.wrong += 1
+      await bot.sendMessage(chatId, 'Nope, try again :)', againOptions)
     }
+
+    await user.save()
   })
 }
 
---
+++ models/User.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require('sequelize')
+
+const sequelize = require('../db')
+
+const User = sequelize.define('user', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
+  chatId: { type: DataTypes.STRING, unique: true, },
+  right: { type: DataTypes.INTEGER, defaultValue: 0 },
+  wrong: { type: DataTypes.INTEGER, defaultValue: 0 },
+})
+
+module.exports = User
